a7a6c9797d1142d80f40076d06520c87c4d5b758
JorgeMzJ <240152@upbc.edu.mx>
2026-04-08 19:36:09 -0700

    Add files via upload

--- Config/App.php
+++ Config/App.php
@@ -9,4 +9,7 @@ if ($base === '') {
 	$base .= '/';
 }
 define('BASE_URL', $base);
+
+define('STRIPE_PUBLIC_KEY', 'pk_test_51TJcCsJqZpMA15CnopcYjB1LTiEBFVTG9QxPFjDLxayUnoK5xBuQTaJss4TmD852aO3C3znUqFnOcbY8lEOuyewV00CfhKyUZK');
+define('STRIPE_SECRET_KEY', 'sk_test_51TJcCsJqZpMA15CnxhYrWgSA2BFvN7auxH1AMsJiVDhwrVBaCBZcxYvZtfiwCmqQxkH346bOc9a36wTh7UxmtAxR00BcXTto3O');
 ?>
